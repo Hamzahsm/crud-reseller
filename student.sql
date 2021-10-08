9921a003c13b737438ba60d524400c53102abd29
Hamzah Saiful Madjid <hamzahsaifulmadjid@gmail.com>
2021-10-08 12:39:15 +0700

    coba update data per id dari open source-

---
+++ student.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `student` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `stud_name` varchar(191) NOT NULL,
+    `stud_class` varchar(100) NOT NULL,
+    `stud_phone` varchar(100) NOT NULL,
+    PRIMARY KEY (`id`)
+);
